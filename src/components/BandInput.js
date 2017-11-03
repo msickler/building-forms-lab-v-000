69e0c7596382bf1f4c2f192bfb9abc7ea0c0bc7c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 23:56:01 +0000

    automatically backed up by learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class BandInput extends Component {
   constructor() {
     super()
-    this.state = { bands: ''}
+    this.state = { band: ''}
   }
 
   handleSubmit(event) {
