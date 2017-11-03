ed9e3e9a520c45a4cb643fcdda244a815dd0f890
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 23:51:13 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Bands extends Component {
   render(){
     const bands = this.props.store.getState().bands.map(band => {
-      return <li>{band}</li>
+      return <li>{band.name}</li>
     })
     return (
       <div>
